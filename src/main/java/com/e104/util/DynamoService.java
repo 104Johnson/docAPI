d1ea30bd17f631e9f22014db80e88cebbead2df6
104Johnson <johnson.chang@104.com.tw>
2016-04-13 15:38:27 +0800

    Dynamo fileid binary decode & encode

--- src/main/java/com/e104/util/DynamoService.java
+++ src/main/java/com/e104/util/DynamoService.java
@@ -1,19 +1,15 @@
 package com.e104.util;
 
-import java.nio.ByteBuffer;
-import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Map.Entry;
 
+import org.apache.commons.codec.DecoderException;
+import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.codec.binary.Hex;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
-
-
-
-
 
 
 import com.amazonaws.auth.profile.ProfileCredentialsProvider;
@@ -27,6 +23,7 @@ import com.amazonaws.services.dynamodbv2.document.spec.UpdateItemSpec;
 import com.amazonaws.services.dynamodbv2.document.utils.NameMap;
 import com.amazonaws.services.dynamodbv2.document.utils.ValueMap;
 import com.amazonaws.services.dynamodbv2.model.ReturnValue;
+import com.amazonaws.util.BinaryUtils;
 import com.e104.errorhandling.DocApplicationException;
 
 public class DynamoService {
@@ -40,12 +37,13 @@ public class DynamoService {
 		return dynamoDB;
 	}
 	
-	public String getItem(String tableName,String fileId) {
+	public String getItem(String tableName,String fileId) throws DecoderException {
 		DynamoDB dynamoDB = dynamoinit();
 		
 		Item userData=null;
 		String rtn="";
-	        PrimaryKey pKey = new PrimaryKey("fileid",ByteBuffer.wrap(fileId.getBytes()));
+	        PrimaryKey pKey = new PrimaryKey("fileid",
+	        		Hex.decodeHex(fileId.toCharArray()));
 	        userData = dynamoDB.getTable(tableName).getItem(pKey);
 	        if(userData!=null && !"".equals(userData))
 	        	rtn = new JSONObject(userData.toJSON()).put("fileid", fileId).toString();
@@ -115,7 +113,7 @@ public class DynamoService {
 			while (iter.hasNext()) { 
 				Map.Entry<String,String> entry = (Map.Entry<String,String>) iter.next(); 
 			    UpdateItemSpec updateItemSpec = new UpdateItemSpec()
-	            .withPrimaryKey("fileid", fileId.getBytes())
+	            .withPrimaryKey("fileid", Hex.decodeHex(fileId.toCharArray()))
 	            .withUpdateExpression("set #key = :value")
 	            .withNameMap(new NameMap()
                 	.with("#key", entry.getKey().toString()))
@@ -125,7 +123,10 @@ public class DynamoService {
 			userData = dynamoDB.getTable(tableName).updateItem(updateItemSpec).getUpdateItemResult().toString();
 			}
 		}catch(NullPointerException e){
+			e.printStackTrace();
 			throw new DocApplicationException("NullPointerException",99);
+		} catch (DecoderException e) {
+			throw new DocApplicationException("Decoder失敗",1);
 		}
 		return userData;
 	}
@@ -143,6 +144,8 @@ public String getItems(String tableName,JSONArray fileIds) throws DocApplication
 				userData.put(new JSONObject(itemData));
 		} catch (JSONException e) {
 			throw new DocApplicationException("Json格式轉換失敗",1);
+		} catch (DecoderException e) {
+			throw new DocApplicationException("Decoder失敗",1);
 		}
 		
 	}
@@ -164,8 +167,8 @@ public String getItems(String tableName,JSONArray fileIds) throws DocApplication
 	} 
 	
 
-	public static void main(String args[]) {
-		System.out.println("Start");
+	public static void main(String args[]) throws DecoderException {
+		/*System.out.println("Start");
 		DynamoService dynamoService = new DynamoService();
 		//JSONArray userData = new JSONArray();
 		//userData.put(new JSONObject("{\"fileid\":\"1e411903e05b4456bcfe01c7288dcde511\"},{\"fileid\":\"906eb1c4667544219607c522fe5332e811\"}"));
@@ -180,7 +183,33 @@ public String getItems(String tableName,JSONArray fileIds) throws DocApplication
 		} catch (DocApplicationException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+		}*/
+		//Decode
+		
+		String fileid ="Iog6sImQR8KNoZad9Nq6shE=";
+		//int b = Integer.decode(S);
+		byte[] fileidByte = Base64.decodeBase64(fileid);
+		System.out.println(Hex.encodeHex(fileidByte));
+		System.out.println(BinaryUtils.toHex(fileidByte));
+		
+		//encode
+		String fileidString = "c7ec8fcbb34347f19980ac88f3d2e1a213";
+		byte[] fileidbyte = Hex.decodeHex(fileidString.toCharArray());
+		System.out.println(Base64.encodeBase64String(fileidbyte));
+		try{
+		System.out.println(BinaryUtils.fromHex("06db2717471e4c769f23996007c514f713"));
+		
+		}catch(Exception e){
+			e.printStackTrace();
 		}
+		
+		//byte[] decodedHex = Hex.decodeHex(S.getBytes());
+		//byte[] encodedHexB64 = Base64.codeBase64(decodedHex);
+		
+		System.out.println();
+		//String bString = new DynamoService().asHex(S.getBytes());
+		//System.out.println(Base64.decodeBase64(S));
+		
 		}
-	
+
 }
