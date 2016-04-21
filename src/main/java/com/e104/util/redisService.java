c52d0969b7764bb0dcca76f8ba6deb30e57ab92f
104Johnson <johnson.chang@104.com.tw>
2016-04-21 10:47:26 +0800

    Delete RedisService

--- src/main/java/com/e104/util/redisService.java
+++ src/main/java/com/e104/util/redisService.java
@@ -1,35 +1,5 @@
 package com.e104.util;
 
-import java.io.IOException;
-import java.net.InetSocketAddress;
-
-import redis.clients.jedis.Jedis;
-
-import com.e104.Errorhandling.DocApplicationException;
-
-import net.spy.memcached.MemcachedClient;
-
 public class RedisService {
-	public MemcachedClient redisClient() throws DocApplicationException{
-		String configEndpoint = "docurlcache.abjn5b.0001.apne1.cache.amazonaws.com";
-        Integer clusterPort = 6379;
 
-        MemcachedClient redis =null;
-        try {
-			redis = new MemcachedClient(
-			        new InetSocketAddress(configEndpoint, 
-			                              clusterPort));
-		} catch (IOException e) {
-			
-			e.printStackTrace();
-			throw new DocApplicationException("redis Error", 14);
-		}
-        return redis;
-		
-	}
-	public Jedis jedisClient(){
-		
-		Jedis jedis = new Jedis("localhost");
-		return jedis;
-	}
 }
