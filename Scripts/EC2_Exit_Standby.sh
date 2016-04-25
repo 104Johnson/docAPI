ee9a8669ef3966ee661c91d59cb317c926933030
104Johnson <johnson.chang@104.com.tw>
2016-04-25 17:30:49 +0800

    Script Modify

--- Scripts/EC2_Exit_Standby.sh
+++ Scripts/EC2_Exit_Standby.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
-AutoScalingGroup="AutoScalingGroup_DocApi"
 REGION="ap-northeast-1"
 
 ID=`curl http://169.254.169.254/latest/meta-data/instance-id`
+AutoScalingGroup=`aws autoscaling --region ap-northeast-1 describe-auto-scaling-instances --instance-ids $ID | grep AutoScalingGroupName | awk -F "\"" '{print $4}'`
+
 STLALTUS=`aws autoscaling describe-auto-scaling-instances --region $REGION --instance-ids $ID | grep -c Standby`
 
 if [ $STLALTUS -eq 1 ];then
